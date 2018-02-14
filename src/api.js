1d6a2a2a3c1b97d2dc6055cc2cc2f8f9c5ab4a3d
DJ Pelland <djp@headwire.com>
2018-02-13 16:16:53 -0800

    move mockData to json

--- src/api.js
+++ src/api.js
@@ -1,17 +1,5 @@
 import axios from 'axios';
 
-const mockData = {
-  "temperature": {
-    "current": 72,
-    "target": 72
-  },
-  "lights": [
-    {"room": "Patio", "switched": "on", "hours": 20},
-    {"room": "Living Room", "switched": "off", "hours": 32},
-    {"room": "Kitchen", "switched": "on", "hours": 112}
-  ]
-}
-
 export const getData = () => {
-  return axios.get('./data.json').then( res => mockData )
+  return axios.get('./data.json').then( res => res.data )
 }
